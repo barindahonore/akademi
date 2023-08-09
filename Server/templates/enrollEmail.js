b7a2e66d655bba7e7d519c4ebbdd2c9f0fa2d45f
Robertisimwe <robertishimwe0@gmail.com>
2023-08-09 10:53:28 +0200

    modified email templ

--- Server/templates/enrollEmail.js
+++ Server/templates/enrollEmail.js
@@ -9,7 +9,7 @@ The course content will be delivered through our user-friendly Learning Manageme
 
 As you embark on this educational journey, remember that learning is a collaborative process. Feel free to engage in discussions, ask questions, and share your insights with your fellow learners. Together, we can create a supportive and vibrant learning community.<br/><br/>
 
-Mark your calendars and make sure to prepare for the mandatory assessment on August 7th, where you will have the chance to showcase your skills in HTML, CSS, and JavaScript. The assessment is a crucial part of the course, and your participation is required to gauge your progress and ensure a successful learning experience.<br/><br/>
+Mark your calendars and make sure to prepare for the mandatory assessment on August 10th, where you will have the chance to showcase your skills in HTML, CSS, and JavaScript. The assessment is a crucial part of the course, and your participation is required to gauge your progress and ensure a successful learning experience.<br/><br/>
 
 If you encounter any technical issues or have questions related to the course content, assignments, or assessments, our dedicated support team is here to assist you. Don't hesitate to reach out to us at edu@melonelab.com.<br/>
 <br/>
@@ -25,7 +25,6 @@ Warm regards,<br/><br/>
 ${instructorName}<br/>
 Instructor<br/>
 Melone Lab Coding Boot Camp<br/>
-${instructorEmail}
 `;
 
 module.exports = enrollEmail;
