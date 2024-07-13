fc3d6eba712cc32222e57241aea8fa2e55843972
barindahonore <106818042+barindahonore@users.noreply.github.com>
2024-07-13 20:09:09 +0000

    add on render link

--- Client/src/constants/bnReference.js
+++ Client/src/constants/bnReference.js
@@ -1,2 +1,6 @@
-export const bnURLref = "https://shy-red-trout-boot.cyclic.app"
-export const baseURLref = 'https://shy-red-trout-boot.cyclic.app'
+// export const bnURLref = "https://shy-red-trout-boot.cyclic.app"
+// export const baseURLref = 'https://shy-red-trout-boot.cyclic.app'
+
+
+export const bnURLref = "https://akademi.onrender.com"
+export const baseURLref = 'https://akademi.onrender.com'
